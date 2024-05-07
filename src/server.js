11c67b74446b2f39132dd4b3ebbc895a149a2db6
FeivelRiodan <feivel1502@gmail.com>
2024-05-08 00:25:16 +0700

    adjust eslint version

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const Hapi = require('@hapi/hapi');
  
 const init = async () => {
   const server = Hapi.server({
-    port: 8000,
+    port: 5000,
     host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
     routes: {
       cors: {
